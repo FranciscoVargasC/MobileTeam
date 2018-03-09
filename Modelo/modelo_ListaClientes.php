7b9c850800a1c7d7df51adfc318b5ee59822daa5
francisco cobain <kurt_marley_music@hotmail.com>
2018-03-09 13:11:54 -0600

    conexionBasica

--- Modelo/modelo_ListaClientes.php
+++ Modelo/modelo_ListaClientes.php
@@ -1,43 +1,61 @@
 <?php
 require_once("../Conection/conectionBD.php");
 
-$conIns = new conection();
-$conn = $conIns -> sqlConection(); 
-
 
 class modelo_ListaClientes{
-
-    private $listaClientes;
+    
+    private $listaClientes;  
 
     public function construct()
         {         
-            $this->$listaClientes = array();            
+            $this->$listaClientes = array(); 
+            $this->$existeError = "";
+            $this->$mensajeError = "";
         }
+    
+  
 
     public function get_ClientsList()
-    {   
-        try{          
-        $params = array(array(1, SQLSRV_PARAM_IN),array(1, SQLSRV_PARAM_IN));
-        $tsql_callSP = "{call SEL_SeleccionarClientesExistentes(?,?)}";
+    {         
+        try{  
+            $conIns = new conection();
+            $conn = $conIns -> sqlConection();     
+           
+            $rol = 1;  
+            $equipo = 1;                    
+            $params = array(array($rol,SQLSRV_PARAM_IN), array($equipo, SQLSRV_PARAM_IN));      
+
+            $tsql_callSP = "{call dbo.SEL_SeleccionarClientesExistentes(?,?)}";
         
-        $stmr = sqlsrv_query($conn, $tsql_callSP, $params);  
-            while($reg = sqlsrv_fetch_array($stmr))
-                {            
-                    $this->listaClientes[] = $reg;
-                }
-                    
-                if($this->listaClientes[0]['ExisteError'] == "0")
-                    {
-                        return $this->listaClientes;
+            $stmr = sqlsrv_query($conn,$tsql_callSP,$params); 
+                
+                while($reg = sqlsrv_fetch_array($stmr))
+                    {   
+                        $this->listaClientes[] = $reg;
                     }
-                    else
-                        {
-                            return null;
-                        }
+                    
+                for($i = 0; $i < count($this->listaClientes); $i++){
+                    $verify = $this->listaClientes[$i]['ExisteError'];
+                    if($verify == "0")
+                    {                        
+                        return $this->listaClientes;
+                        break;
+                    }
+                    else if($verify == "2")
+                    {                                   
+                        return $this->listaClientes[$i]['MensajeError'];
+                        break;
+                    }else{
+                        return "Se ha producido un error indefinido, favor de contactar con el administrador";
+                        break;
+                    }
+                }
                 
         }catch(Exception $ex){
-            return "Se ha producido un error generico, favor de contactar con el administrador";
+            return "Se ha producido un error indefinido, favor de contactar con el administrador";
         }        
     }//get_ClientsList
+
+    
 }//class
 ?>
