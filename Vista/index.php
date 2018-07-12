1f3f70ef031c6664a353af973dbb0748149029ba
Oscar <oibarra@anzen.com.mx>
2018-07-12 15:56:22 -0500

    LoginChangesViews

--- Vista/index.php
+++ Vista/index.php
@@ -2,16 +2,95 @@
     require_once("../Vista/loginValidation.php");
 ?>
 
-<html>
+
+<html lang="es">
 <head>
- <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
- <title>Mobile QA</title>
+	<title>Login Repocitorio CIT</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
- <form method="post" action="../Controlador/InicioSession.php" autocomplete="off">
-  Usuario: <input type="text" name="nombUsuario" id="usuario" title="Ingresa tu usuario" required>
-  Contraseña: <input type="password" name="pass" id="password" title="Ingresa tu contraseña" required>  
-  <input type="submit" onclick = "validation_login()" value="Ingresar">
- </form>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="images/logo_anzen.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" method="post" action="../Controlador/InicioSession.php">
+					<span class="login100-form-title">
+						Login Tester
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="nombUsuario" placeholder="Usuario" id="usuario" required>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password" required> 
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+                        <input type="submit" onclick = "validation_login()" value="Ingresar" class="login100-form-btn">
+					</div>
+
+					
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Crear Usuario
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
